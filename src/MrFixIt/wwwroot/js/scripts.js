b95cb7e863fd5134481ff2a2a7f12fac9d5a9a32
rouz majlessi <rmajlessi@gmail.com>
2016-12-09 22:19:55 -0800

    fix merge issues

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,15 @@
 ﻿var test = "it works";
+
+
+$(".job-claim").submit(function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: "/Claim/Jobs",
+        type: "POST",
+        dataType: "json",
+        data: $(this).serialize(),
+        success: function (result) {
+            $("#Claimed").html(result);
+        }
+    });
+});
